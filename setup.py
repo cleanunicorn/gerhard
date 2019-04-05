a68165f709bff5962e383d7e14a4621ed1a97333
Daniel Luca <lucadanielcostin@gmail.com>
2019-04-05 16:10:56 +0200

    [WIP] Rewriting to include click cli framework

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='gerhard',
+    version='0.1',
+    py_modules=['gerhard'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        gerhard=gerhard.main:cli
+    ''',
+)
